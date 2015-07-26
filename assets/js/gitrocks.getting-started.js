02cffd96c844457a944b5cc1bdcb4e2a34409926
Vincent Lamanna <vincent@salesfloor.net>
2015-07-26 15:46:59 -0400

    Small refactor.

---
+++ assets/js/gitrocks.getting-started.js
@@ -0,0 +1,14 @@
+var gitRocks = {
+    currentTutorial: "gettingStarted",
+    currentStep: 0,
+    gettingStarted: {
+        step0: {
+            command: "git init",
+            success: "You just created your first repository. Now let's add some files."
+        },
+        step1: {
+            command: "touch README.md",
+            success: "You created a README file. Now let's commit it."
+        }
+    }
+};
